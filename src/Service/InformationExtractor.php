fa8951feb216401e098e29a66b71d877f6042dbe
Giacomo Moscardini <gmoscardini@webgriffe.com>
2020-02-19 15:23:03 +0100

    [REFACTOR] throw exception when extraction fails

--- src/Service/InformationExtractor.php
+++ src/Service/InformationExtractor.php
@@ -3,39 +3,50 @@ declare(strict_types=1);
 
 namespace LamasFoker\LiveQuiz\Service;
 
+use LamasFoker\LiveQuiz\Exception\TextExtractionException;
+
 class InformationExtractor
 {
     /**
      * @param string $text
      * @return string
+     * @throws TextExtractionException
      */
     public function extractQuestion(string $text): string
     {
-        $start = strpos($text, '€');
-        if ($start !== false) {
-            $text = mb_substr($text, ++$start);
+        $start = mb_strpos($text, '€' . PHP_EOL);
+        if ($start === false) {
+            throw new TextExtractionException('Char "€" not found in: '.PHP_EOL.$text);
         }
-        $end = strpos($text, '?');
-        if ($start !== false) {
-            $text = mb_substr($text, 0, $end);
+        $text = mb_substr($text, ++$start);
+        $end = mb_strpos($text, '?' . PHP_EOL)?:strpos($text, ':' . PHP_EOL);
+        if ($end === false) {
+            throw new TextExtractionException('Char "?" or ":" not found in: '.PHP_EOL.$text);
         }
+        $text = mb_substr($text, 0, ++$end);
+        $text = str_replace(PHP_EOL, ' ', $text);
+        $text = str_replace('"', '', $text);
         return $text;
     }
 
+
     /**
      * @param string $text
-     * @return string[]
+     * @return array
+     * @throws TextExtractionException
      */
     public function extractAnswers(string $text): array
     {
-        $start = strpos($text, '?');
-        if ($start !== false) {
-            $text = mb_substr($text, --$start);
+        $start = mb_strpos($text, '?' . PHP_EOL)?:strpos($text, ':' . PHP_EOL);
+        if ($start === false) {
+            throw new TextExtractionException('Char "?" or ":" not found in: '.PHP_EOL.$text);
         }
+        $text = mb_substr($text, --$start);
         $answers = explode(PHP_EOL, $text);
-        $answers = array_filter($answers, function($key) {
-            return $key<3;
-        }, ARRAY_FILTER_USE_KEY);
+        $answers = array_filter($answers, function($value) {
+            return mb_strlen($value) > 2;
+        });
+        $answers = array_values($answers);
         return $answers;
     }
 }
